da056eae1d9e50748d05737bc118bb1ac5d1ecc8
Tural <tural.isazada@gmail.com>
2025-07-13 13:39:27 +0200

    added template to repo

---
+++ template.py
@@ -0,0 +1,29 @@
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <p id='messages'></p>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws/123");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                messages.innerText+=event.data
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
